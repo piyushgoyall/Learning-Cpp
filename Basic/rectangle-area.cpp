b6ed3bb67c5cbf83f83ee25221848c50dadf5fe3
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-30 22:50:20 +0530

    Add files via upload

---
+++ Basic/rectangle-area.cpp
@@ -0,0 +1,15 @@
+//wap to enter length and breadth of a rectangle and find its area.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int length,breadth,area;
+	cout<<"Enter the length of rectangle : ";
+	cin>>length;
+	cout<<"\n";
+	cout<<"Enter the breadth of rectangle : ";
+	cin>>breadth;
+	cout<<"\n";
+	area=length*breadth;
+	cout<<"Area of the rectangle is : "<<area;
+}
