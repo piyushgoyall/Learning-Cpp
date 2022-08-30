47de84ef04bedbb2ad683baed9d8cbcd23f46911
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-30 21:49:04 +0530

    Add files via upload

---
+++ Basic/average.cpp
@@ -0,0 +1,18 @@
+//wap to calculate the average of three numbers 
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d;
+	cout<<"Enter the first number : ";
+	cin>>a;
+	cout<<"\n";
+	cout<<"Enter the second number : ";
+	cin>>b;
+	cout<<"\n";
+	cout<<"Enter the third number : ";
+	cin>>c;
+	cout<<"\n";
+	d=(a+b+c)/3;
+	cout<<"Average of the three numbers : "<<d;
+}
