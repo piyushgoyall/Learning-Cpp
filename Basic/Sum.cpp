6fd5f7c0116e94699ffa45fb7c110eb77e3edd07
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-30 21:47:33 +0530

    Add files via upload

---
+++ Basic/Sum.cpp
@@ -0,0 +1,15 @@
+//wap to enter two numbers and find their sum with the third variable 
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the first number : ";
+	cin>>a;
+	cout<<"\n";
+	cout<<"Enter the second number : ";
+	cin>>b;
+	cout<<"\n";
+	c=a+b;
+	cout<<"Sum of entered numbers : "<<c;
+}
