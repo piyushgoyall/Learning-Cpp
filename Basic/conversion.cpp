1c8eac2f3f48d3fa48e91428a699de5d1b88e353
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-31 07:49:06 +0530

    Add files via upload

---
+++ Basic/conversion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float cm,m,km;
+	cout<<"Enter the centimeter value : ";
+	cin>>cm;
+	cout<<"\n";
+	m=cm/100;
+	cout<<"Centimeter to meter value : "<<m;
+	cout<<"\n";
+	cout<<"\n";
+	km=cm/100000;
+	cout<<"Centimeter to kilometer value : "<<km;
+}
