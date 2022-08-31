8d2720ceb49534473dec09e04f417280e5ed79a7
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-31 09:41:36 +0530

    Add files via upload

---
+++ Basic/profit-loss.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float cp,sp,profit,loss,lper,pper;
+	cout<<"Enter the Cost price : ";
+	cin>>cp;
+	cout<<"\n";
+	cout<<"Enter the selling price : ";
+	cin>>sp;
+	cout<<"\n";
+	if(cp>sp)
+	{
+		loss=cp-sp;
+		lper=(loss*100)/cp;
+		cout<<"Percentage loss : "<<lper;
+		cout<<"%";
+	}
+	if(sp>cp)
+	{
+		profit=sp-cp;
+		pper=(profit*100)/cp;
+		cout<<"Percentage profit : "<<pper;
+		cout<<"%";
+	}
+}
