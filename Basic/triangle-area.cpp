04e3d4396110a39085888096644443e165fab1f9
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-31 09:31:57 +0530

    Add files via upload

---
+++ Basic/triangle-area.cpp
@@ -0,0 +1,16 @@
+//wap to enter the base and height of triangle and find its area.
+#include<iostream>
+using namespace std;
+int main()
+{
+	float base,height,area;
+	cout<<"Enter the Height : ";
+	cin>>height;
+	cout<<"\n";
+	cout<<"Enter the base : ";
+	cin>>base;
+	cout<<"\n";
+	area=(base*height)/2;
+	cout<<"Area of the triangle : "<<area;
+	cout<<" sq. units";
+}
