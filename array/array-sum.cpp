c412f434c4a2a86aafa08824530f052ebb59ebe8
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-10 16:51:01 +0530

    Add files via upload

---
+++ array/array-sum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class array
+{
+	public:
+		void sum()
+		{
+			int a[5],b[5],c[5],pos;
+			for(pos=0;pos<5;pos++)
+			{
+				cout<<"Enter the element for a["<<pos<<"] : ";
+				cin>>a[pos];
+			}
+			cout<<"\n";
+			for(pos=0;pos<5;pos++)
+			{
+				cout<<"Element of a["<<pos<<"] : "<<a[pos];
+				cout<<"\n";
+			}
+			cout<<"\n";
+			for(pos=0;pos<5;pos++)
+			{
+				cout<<"Enter the element for b["<<pos<<"] : ";
+				cin>>b[pos];
+			}
+			cout<<"\n";
+			for(pos=0;pos<5;pos++)
+			{
+				cout<<"Element of b["<<pos<<"] : "<<b[pos];
+				cout<<"\n";
+			}
+			cout<<"\n";
+			for(pos=0;pos<5;pos++)
+			{
+				c[pos]=a[pos]+b[pos];
+				cout<<"Element of c["<<pos<<"] : "<<c[pos];
+				cout<<"\n";
+			}
+		}
+};
+int main()
+{
+	array ob;
+	ob.sum();
+}
