eac0f5aed0a53b86d90bf6c3570138561d5feff8
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-16 18:11:18 +0530

    Add files via upload

---
+++ array/matrix/matrix-lower-triangular.cpp
@@ -0,0 +1,62 @@
+//wap to find upper triangular matrix.
+#include<iostream>
+using namespace std;
+class mat 
+{
+	public:
+		void upper_triangular()
+		{
+			int n;
+			cout<<"Enter the size of the matrix : ";
+			cin>>n;
+			cout<<"\n";
+			int a[n][n],r,c;
+			for(r=0;r<n;r++)
+			{
+				for(c=0;c<n;c++)
+				{
+					cout<<"Enter the element for a["<<r<<"]["<<c<<"] : ";
+					cin>>a[r][c];
+				}
+			}
+			cout<<"\n";
+			cout<<"Entered matrix : ";
+			cout<<"\n";
+			cout<<"\n";
+			for(r=0;r<n;r++)
+			{
+				for(c=0;c<n;c++)
+				{
+					cout<<a[r][c];
+					cout<<"\t";
+				}
+				cout<<"\n";
+			}
+			cout<<"\n";
+			for(r=0;r<n;r++)
+			{
+				for(c=0;c<n;c++)
+				{
+					if(c<=r)
+					{
+						cout<<a[r][c];
+					}
+					if(c>r)
+					{
+						cout<<"0";
+						cout<<"\t";
+					}
+					else
+					{
+						cout<<"\t";
+					}
+				}
+				cout<<"\n";
+			}
+		}
+};
+int main()
+{
+	mat ob;
+	ob.upper_triangular();
+}
