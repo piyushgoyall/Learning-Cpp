491ff864d690d5cf3b0d5019434c5fffcbf808e0
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-17 09:22:54 +0530

    Update array-max-min-recursion.cpp

--- array/recursion/array-max-min-recursion.cpp
+++ array/recursion/array-max-min-recursion.cpp
@@ -5,24 +5,24 @@ int array_max(int arr[], int start, int N);
 int array_min(int arr[], int start, int N);
 int main()
 {
-    int arr[100];
-    int N, i;
-    cout<<"Enter size of the array: ";
-    cin>>N;
-    cout<<"\n";
-    for(i=0; i<N; i++) 
-    {
-        cout<<"Enter element for arr["<<i<<"] : ";
+    	int arr[100];
+    	int N, i;
+    	cout<<"Enter size of the array: ";
+    	cin>>N;
+    	cout<<"\n";
+    	for(i=0; i<N; i++) 
+    	{
+        	cout<<"Enter element for arr["<<i<<"] : ";
 		cin>>arr[i];
-    }
-    cout<<"\n";
-    cout<<"Maximum element : ";
-    int k = array_max(arr, 0, N);
-    cout<<k;
-    cout<<"\n";
-    cout<<"Minimum element : ";
-    int p = array_min(arr, 0, N);
-    cout<<p;
+    	}
+    	cout<<"\n";
+   	cout<<"Maximum element : ";
+   	int k = array_max(arr, 0, N);
+    	cout<<k;
+    	cout<<"\n";
+    	cout<<"Minimum element : ";
+    	int p = array_min(arr, 0, N);
+    	cout<<p;
 }
 int array_max(int arr[], int start, int N)
 {
@@ -31,7 +31,7 @@ int array_max(int arr[], int start, int N)
   	{
   		if(arr[start]>max)
 		{
-		    max=arr[start];
+		    	max=arr[start];
   			array_max(arr,start+1,N);
   		}
   		else
