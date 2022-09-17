cc7e3f99ba00241784da55e763ddd2361d02b229
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-17 09:19:36 +0530

    Update array-recursion.cpp

--- array/recursion/array-recursion.cpp
+++ array/recursion/array-recursion.cpp
@@ -4,21 +4,21 @@ using namespace std;
 void printArray(int arr[], int start, int N);
 int main()
 {
-    int arr[100];
-    int N, i;
-    cout<<"Enter size of the array: ";
-    cin>>N;
-    cout<<"\n";
-    for(i=0; i<N; i++) 
-    {
-        cout<<"Enter element for arr["<<i<<"] : ";
+    	int arr[100];
+    	int N, i;
+    	cout<<"Enter size of the array: ";
+    	cin>>N;
+    	cout<<"\n";
+    	for(i=0; i<N; i++) 
+    	{
+        	cout<<"Enter element for arr["<<i<<"] : ";
 		cin>>arr[i];
-    }
-    cout<<"\n";
-    cout<<"Elements in the array: ";
-    cout<<"\n";
-    cout<<"\n";
-    printArray(arr, 0, N);
+    	}
+    	cout<<"\n";
+    	cout<<"Elements in the array: ";
+    	cout<<"\n";
+    	cout<<"\n";
+    	printArray(arr, 0, N);
 }
 void printArray(int arr[], int start, int N)
 {
