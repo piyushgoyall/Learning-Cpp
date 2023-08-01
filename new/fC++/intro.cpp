ef21e2a96cb60605066de10cde52a1f4d289ca73
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-01 23:02:01 +0530

    new

---
+++ new/fC++/intro.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a; //decleration 
+    a = 12; // initialization 
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+
+    short int si;
+    long int li;
+    cout<<"size of short int "<<sizeof(si)<<endl;
+    cout<<"size of long int "<<sizeof(li)<<endl;
+    return 0;
+}
