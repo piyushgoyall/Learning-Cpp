92eb8f26c15c9db79d16bc217303b636604fe5c7
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-07 23:31:06 +0530

    5

---
+++ new/fC++/13_Pattern_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            if(i==0 || i==row-1 || j==0 || j == col-1)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
