438676b2c05ce96d8eb8ac045de2f86bd203ddcf
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-12 23:34:39 +0530

    9

---
+++ new/fC++/20_Pattern_9.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row;
+    cin>>row;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            if((i+j)%2==0)
+            {
+                cout<<1;
+            }
+            else
+            {
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
