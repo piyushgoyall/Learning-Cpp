0daf8b1c148ff4f78c70b2e6467d2a7940162c36
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-15 01:52:49 +0530

    12

--- new/fC++/23_Pattern_12.cpp
+++ new/fC++/23_Pattern_12.cpp
@@ -1,3 +1,10 @@
+// 5
+//         1
+//       2 1 2
+//     3 2 1 2 3
+//   4 3 2 1 2 3 4
+// 5 4 3 2 1 2 3 4 5
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
---
+++ new/fC++/24_Pattern_13.cpp
@@ -0,0 +1,44 @@
+// 5
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row;
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= (2 * i) - 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = row; i >= 1; i--)
+    {
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= (2 * i) - 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
