da8e10ea779c960537bb594314ed40252f6a7ee9
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-14 19:15:18 +0530

    11

---
+++ new/fC++/23_Pattern_12.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row;
+    cin>>row;
+    for(int i=1;i<=row;i++)
+    {
+        int t=i;
+        for(int j=1;j<=row-i;j++)
+        {
+            cout<<"  ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout<<t<<" ";
+            t-=1;
+        }
+        for(int j=1;j<i;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
