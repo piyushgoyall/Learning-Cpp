08ef3418d9d13e49aa12084b04821b69050497ca
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-18 16:40:15 +0530

    15

--- new/fC++/1_syntax/26_Check_prime.cpp
+++ new/fC++/1_syntax/26_Check_prime.cpp
@@ -1,21 +1,45 @@
 #include <bits/stdc++.h>
+#include<cmath>
 using namespace std;
 int main()
 {
-    int num;
-    cin>>num;
-    int i;
-    for(i=2;i<num;i++)
+    int n;
+    cin>>n;
+    bool flag=0;
+    for(int i=2;i<=sqrt(n);i++)
     {
-        if(num%i==0)
+        if(n%i==0)
         {
-            cout<<"Non-Prime";
+            cout<<"Non-Prime"<<endl;
+            flag = 1;
             break;
         }
     }   
-    if(num==i)
+    if(flag==0)
     {
         cout<<"Prime"<<endl;
     }
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cin>>num;
+//     int i;
+//     for(i=2;i<num;i++)
+//     {
+//         if(num%i==0)
+//         {
+//             cout<<"Non-Prime";
+//             break;
+//         }
+//     }   
+//     if(num==i)
+//     {
+//         cout<<"Prime"<<endl;
+//     }
+//     return 0;
+// }
