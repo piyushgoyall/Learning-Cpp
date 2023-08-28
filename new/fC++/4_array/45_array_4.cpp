74245503d734a135b615afea327110d90f2bb0cf
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-08-28 15:00:49 +0530

    21

--- new/fC++/4_array/45_array_4.cpp
+++ new/fC++/4_array/45_array_4.cpp
@@ -3,11 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int linearSearch(int arr[],int n,int x)
+int linearSearch(int arr[], int n, int x)
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i]==x)
+        if (arr[i] == x)
         {
             return i;
         }
@@ -17,15 +17,15 @@ int linearSearch(int arr[],int n,int x)
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
     int x;
-    cout<<"Enter element to be searched: ";
-    cin>>x;
-    cout<<linearSearch(arr,n,x);
+    cout << "Enter element to be searched: ";
+    cin >> x;
+    cout << linearSearch(arr, n, x);
     return 0;
 }
