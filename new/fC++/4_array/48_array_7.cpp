590282f51695a2e7482d4eaf81a91a07fde28b2a
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-09-05 02:04:47 +0530

    25

---
+++ new/fC++/4_array/48_array_7.cpp
@@ -0,0 +1,32 @@
+// Selection Sort
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
