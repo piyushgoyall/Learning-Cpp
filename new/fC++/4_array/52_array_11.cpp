08cd6d70554fb3227c9439441ae556b335be5e21
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-09-07 12:14:53 +0530

    1

---
+++ new/fC++/4_array/52_array_11.cpp
@@ -0,0 +1,25 @@
+// Sum of all subarrays;
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int sum;
+    for (int i = 0; i < n; i++)
+    {
+        sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            cout << sum << endl;
+        }
+    }
+    return 0;
+}
---
+++ new/fC++/4_array/53_array_12.cpp
@@ -0,0 +1,34 @@
+// Longest Arithematic Array
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ans = 2;
+    int pd = arr[1] - arr[0];
+    int j = 2;
+    int curr = 2;
+    while (j < n)
+    {
+        if (pd == arr[j] - arr[j - 1])
+        {
+            curr++;
+        }
+        else
+        {
+            pd = arr[j] - arr[j - 1];
+            curr = 2;
+        }
+        ans = max(ans, curr);
+        j++;
+    }
+    cout << "Size Of Longest Arithematic Array: " << ans << endl;
+    return 0;
+}
