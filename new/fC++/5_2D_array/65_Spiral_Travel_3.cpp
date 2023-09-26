707b710cbb39d9bafd8c0f69bdac4292d913a6db
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-09-26 20:20:17 +0530

    new

---
+++ new/fC++/5_2D_array/65_Spiral_Travel_3.cpp
@@ -0,0 +1,26 @@
+// Spiral Order matrix traversal.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int rs = 0;
+    int cs = m-1;
+    int re = n-1;
+    int ce = 0;
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    return 0;
+}
