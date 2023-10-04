c69e4c66bdb2c1db96f1dff0e4cad20b1b17ff1a
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-10-05 00:32:57 +0530

    new

--- new/fC++/chararcter_array/72_largest_word_4.cpp
+++ new/fC++/chararcter_array/72_largest_word_4.cpp
@@ -5,21 +5,27 @@ using namespace std;
 int main()
 {
     char arr[1000];
-    cin >> arr;
+    cin.getline(arr, 1000);
     int len = strlen(arr);
     int premax = 0, curlen = 0;
+    int lstart = 0, lend = 0;
     for (int i = 0; i < len; i++)
     {
-        curlen += 1;
-        cout << "- " << curlen << " -" << endl;
+
         if (arr[i] == ' ')
         {
             if (curlen > premax)
             {
                 premax = curlen;
-                cout << "- " << premax << " -" << endl;
+                lstart=i+1;
+                // cout << "- " << premax << " -" << endl;
             }
             curlen = 0;
+        }
+        else
+        {
+            curlen += 1;
+            // cout << "-- " << curlen << " --" << endl;
         }
     }
     cout << "Largest word length: " << premax;
