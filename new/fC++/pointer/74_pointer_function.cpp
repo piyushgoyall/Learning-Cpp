772a95a4186f870fca7d3352ee1d88aa22338b34
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-10-10 17:27:41 +0530

    new

---
+++ new/fC++/pointer/74_pointer_function.cpp
@@ -0,0 +1,17 @@
+// Swap two values
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swap(&a, &b);
+    cout << a << " " << b;
+    return 0;
+}
