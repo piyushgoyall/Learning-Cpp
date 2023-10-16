b00ee762c2ddd20b5cf62ffdf9d8d10167ff5d99
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-10-17 01:42:44 +0530

    imp str functions

---
+++ new/fC++/8_string/77_str_compare.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "weroognfmcoexocp";
+    sort(s1.begin(), s1.end());
+    cout << s1 << endl;
+
+    // int x = 786;
+    // cout << to_string(x) + "2" << endl;
+
+    // string s1 = "786";
+    // int x = stoi(s1);
+    // cout << x + 2 << endl;
+
+    // string s1 = "nincompoop";
+
+    // string s = s1.substr(6, 4);
+    // cout << s << endl;
+
+    // for (int i = 0; i < s1.length(); i++)
+    // {
+    //     cout << s1[i] << endl;
+    // }
+
+    // s1.insert(2, "lol");
+    // cout << s1 << endl;
+
+    // s1.erase(3, 3);
+    // cout << s1 << endl;
+
+    // cout << s1.find("poop") << endl;
+
+    // string s1 = "abc";
+    // string s2 = "abc";
+    // if (s1.compare(s2) == 0)
+    // {
+    //     cout << "String are equal" << endl;
+    // }
+    // cout << s2.compare(s1) << endl;
+
+    // s1.clear();
+    // if (!s1.empty())
+    // {
+    //     cout << "String is not empty" << endl;
+    // }
+
+    return 0;
+}
