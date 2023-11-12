6990cd1f6c3c5a41f59eb211d1d1c0deaab56e34
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-11-12 11:49:48 +0530

    new

--- new/fC++/9_bit_manipulation/90_array_2_unique.cpp
+++ new/fC++/9_bit_manipulation/90_array_2_unique.cpp
@@ -2,12 +2,40 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int set Bit(inr arr,int nn)
+int setBit(int n, int pos)
 {
-    return ()
+    return ((n & (1 << pos)) != 0);
+}
+void unique(int arr[], int n)
+{
+    int xorsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        xorsum = xorsum ^ arr[i];
+    }
+    int tempxor = xorsum;
+    int setbit = 0;
+    int pos = 0;
+    while (setbit != 1)
+    {
+        setbit = xorsum & 1;
+        pos++;
+        xorsum = xorsum >> 1;
+    }
+    int newxor = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (setBit(arr[i], pos - 1))
+        {
+            newxor = newxor ^ arr[i];
+        }
+    }
+    cout << newxor << endl;
+    cout << (tempxor ^ newxor) << endl;
 }
 int main()
 {
-    
+    int arr[] = {1, 2, 3, 3, 2, 1, 4, 7};
+    unique(arr, 8);
     return 0;
 }
