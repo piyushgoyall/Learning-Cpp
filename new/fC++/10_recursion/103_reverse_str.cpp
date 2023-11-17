6ef5ded220dc72a331c07e8fa7041da62ccd7cbc
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-11-17 22:11:48 +0530

    new

--- new/fC++/10_recursion/103_reverse_str.cpp
+++ new/fC++/10_recursion/103_reverse_str.cpp
@@ -4,10 +4,18 @@
 using namespace std;
 void rev(string s)
 {
-    
+    if (s.length() == 0)
+    {
+        return;
+    }
+    string ros = s.substr(1);
+    rev(ros);
+    cout << s[0];
 }
 int main()
 {
-    
+    string s;
+    cin >> s;
+    rev(s);
     return 0;
 }
