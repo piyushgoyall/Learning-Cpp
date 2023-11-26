41865593da3934a24166d28f7c47c148f730bcf9
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-11-27 00:01:46 +0530

    new

--- new/fC++/12_backtracking/121_N_Queen_Problem.cpp
+++ new/fC++/12_backtracking/121_N_Queen_Problem.cpp
@@ -64,25 +64,32 @@ int main()
     int n;
     cin >> n;
 
-    int **arr = new int *[n];
-    for (int i = 0; i < n; i++)
+    if (n > 3)
     {
-        arr[i] = new int[n];
-        for (int j = 0; j < n; j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
-    if (nQueen(arr, 0, n))
-    {
+        int **arr = new int *[n];
         for (int i = 0; i < n; i++)
         {
+            arr[i] = new int[n];
             for (int j = 0; j < n; j++)
             {
-                cout << arr[i][j] << " ";
+                arr[i][j] = 0;
             }
-            cout << endl;
         }
+        if (nQueen(arr, 0, n))
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    cout << arr[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "NO POSSIBLE WAY!!" << endl;
     }
     return 0;
 }
