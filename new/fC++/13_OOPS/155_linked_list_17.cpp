e9334d87e807fc3797e4f66350e64bff1c6d5fed
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-12-19 15:16:01 +0530

    new

---
+++ new/fC++/13_OOPS/155_linked_list_17.cpp
@@ -0,0 +1,86 @@
+// Put even position nodes after odd position nodes.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int val)
+{
+    Node *n = new Node(val);
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(Node *&head, int val)
+{
+    Node *n = new Node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void evenAfterOdd(Node *&head)
+{
+    Node *odd = head;
+    Node *even = head->next;
+    Node *evenStart = even;
+    while (odd->next != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = evenStart;
+    if (odd->next != NULL)
+    {
+        even->next = NULL;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    for (int i = 0; i < 6; i++)
+    {
+        insertAtTail(head, arr[i]);
+    }
+    display(head);
+    evenAfterOdd(head);
+    display(head);
+    return 0;
+}
