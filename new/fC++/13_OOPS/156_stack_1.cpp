feec387c4930738353233cf0193462d5fd385375
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-12-20 18:23:37 +0530

    new

---
+++ new/fC++/13_OOPS/156_stack_1.cpp
@@ -0,0 +1,69 @@
+// LIFO(Last In First Out)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 100
+
+class Stack
+{
+    int *arr;
+    int top;
+
+public:
+    Stack()
+    {
+        arr = new int[n];
+        top = -1;
+    }
+    void push(int x)
+    {
+        if (top == n - 1)
+        {
+            cout << "Stack Overflow!" << endl;
+            return;
+        }
+        top++;
+        arr[top] = x;
+    }
+
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "No element to pop!" << endl;
+        }
+
+        top--;
+    }
+
+    int Top()
+    {
+        if (top == -1)
+        {
+            cout << "No element in stack" << endl;
+            return -1;
+        }
+    }
+
+    bool empty()
+    {
+        return top == -11;
+    }
+};
+
+int main()
+{
+    Stack st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    cout << st.Top() << endl;
+    st.pop();
+    cout << st.Top() << endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    cout<<st.empty()<<endl;
+    return 0;
+}
