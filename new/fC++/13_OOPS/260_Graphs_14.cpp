d86c06279740948431187201b4984cdf041094f8
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-02-09 23:49:15 +0530

    new

---
+++ new/fC++/13_OOPS/260_Graphs_14.cpp
@@ -0,0 +1,90 @@
+// Prims Algorithm
+// Idea
+// Select an arbitrary vertex s to start the tree from.
+//      While(there are still nontree vertices)
+//          Select the edge of minimum weight between a tree and nontree vertex.
+//          Add the selected edge and vertex to the tree.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+const int N = 1e5 + 3;
+vector<pair<int, int>> g[N];
+int cost = 0;
+vector<int> dist(N), parent(N);
+vector<bool> vis(N);
+const int INF = 1e9;
+
+void primsMST(int source)
+{
+    for (int i = 0; i < n; i++)
+    {
+        dist[i] = INF;
+    }
+    set<vector<int>> s;
+    dist[source] = 0;
+    s.insert({0, source});
+    // {wt,vertex}
+    while (!s.empty())
+    {
+        auto x = *(s.begin());
+        // s.erase(x);
+        s.erase(s.begin());
+        vis[x[1]] = true;
+        int u = x[1];
+        int v = parent[x[1]];
+        int w = x[0];
+        cout << u << " " << v << " " << w << "\n";
+        cost += w;
+
+        for (auto it : g[x[1]])
+        {
+            if (vis[it.first])
+                continue;
+            if (dist[it.first] > it.second)
+            {
+                s.insert({it.second, it.first});
+                dist[it.first] = it.second;
+                parent[it.first] = x[1];
+            }
+        }
+
+        // for (auto it : g[x[1]])
+        // {
+        //     // if (vis[it.first])
+        //     //     continue;
+        //     // if (dist[it.first] > it.second)
+        //     // {
+        //     //     s.insert({it.second, it.first});
+        //     //     dist[it.first] = it.second;
+        //     //     parent[it.first] = x[1];
+        //     // }
+
+        //     if (vis[it[0]])
+        //         continue;
+        //     if (dist[it[0]] > it[1])
+        //     {
+        //         s.erase({dist[it[0]], it[0]});
+        //         dist[it[0]] = it[1];
+        //         s.insert({dist[it[0]], it[0]});
+        //         parent[it[0]] = x[1];
+        //     }
+        
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
+    }
+    primsMST(0);
+    cout << cost;
+    return 0;
+}
---
+++ new/fC++/13_OOPS/261_Graphs_15.cpp
@@ -0,0 +1,6 @@
+// Dijkstra's Algorithm
+// Problem
+// Given a weighted graph. Find the single source shortest path from a vertex u to all the vertices in the graph. 
+// If the node is unreachable, then print -1.
+
+// NOTE: The weights of the edges must be positive.
