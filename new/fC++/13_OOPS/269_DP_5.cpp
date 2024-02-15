ab11ba2d905698602ead4d8e2160ba69414bd153
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-02-15 23:34:06 +0530

    new

--- new/fC++/13_OOPS/269_DP_5.cpp
+++ new/fC++/13_OOPS/269_DP_5.cpp
@@ -26,6 +26,8 @@
 // 3) Output the answer
 // Time Complexity: O(n * w)
 
+// MEMOIZATION
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -38,17 +40,99 @@ using namespace std;
 #define ss second
 #define setBits(x) builtin_popcount(x)
 
-const int N = 1e5 + 2, MOD = 1e9 + 7;
+const int N = 1e3 + 2, MOD = 1e9 + 7;
 int val[N], wt[N];
+int dp[N][N];
+
+int Knapsack(int n, int w)
+{
+    if (w <= 0)
+        return 0;
+
+    if (n <= 0)
+        return 0;
+
+    if (dp[n][w] != -1)
+        return dp[n][w];
+
+    if (wt[n - 1] > w)
+        dp[n][w] = Knapsack(n - 1, w);
+
+    else
+        dp[n][w] = max(Knapsack(n - 1, w), Knapsack(n - 1, w - wt[n - 1]) + val[n - 1]);
+
+    return dp[n][w];
+}
 
 signed main()
 {
-    int n;
-    cin>>n;
-
-    rep(i,0,n)
+    rep(i, 0, N)
     {
-
+        rep(j, 0, N)
+            dp[i][j] = -1;
     }
+
+    int n;
+    cin >> n;
+
+    rep(i, 0, n)
+    {
+        cin >> wt[i];
+    }
+    rep(i, 0, n)
+            cin >>
+        val[i];
+    int w;
+    cin >> w;
+
+    cout << Knapsack(n, w) << endl;
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define vi vector<int>
+// #define vvi vector<vi>
+// #define pii pair<int, int>
+// #define vii vector<pii>
+// #define rep(i, a, b) for (int i = 0; i < b; i++)
+// #define ff first
+// #define ss second
+// #define setBits(x) builtin_popcount(x)
+
+// const int N = 1e5 + 2, MOD = 1e9 + 7;
+// int val[N], wt[N];
+
+// int Knapsack(int n, int w)
+// {
+//     if (w <= 0)
+//         return 0;
+
+//     if (n <= 0)
+//         return 0;
+
+//     if (wt[n - 1] > w)
+//         return Knapsack(n - 1, w);
+
+//     return max(Knapsack(n - 1, w), Knapsack(n - 1, w - wt[n - 1]) + val[n - 1]);
+// }
+
+// signed main()
+// {
+// int n;
+// cin >> n;
+
+// rep(i, 0, n)
+// {
+//     cin >> wt[i];
+// }
+// rep(i, 0, n)
+//         cin >>
+//     val[i];
+// int w;
+// cin >> w;
+
+// cout << Knapsack(n, w) << endl;
+// return 0;
+// }
---
+++ new/fC++/13_OOPS/270_DP_6.cpp
@@ -0,0 +1,66 @@
+// Longest Increasing Subsequence
+// Problem: You are given an array. Find the length of longest increasing subsequence.
+// Subarray: Continuous block of elements
+// Subsequence: Part of the arry in order. It may or may not be continuous.
+// Every subarray is a subsequence but not every subsequence is not a subarray.
+// Ex: [1, 4, 2, 5, 3]   Can be: {1, 4, 5} & {1, 2, 3}
+//                          Ans: 3
+
+// Approach: (Using Tabulation)
+// 1) Make a dp array and initialize all the dp(i) by 1.
+//    {Since single element is also an Lis}
+// 2) For every i from left to right, iterate from j = 0 to j = i - 1
+// Checking
+//      If (a[i] > a[j])
+//          dp[i] = max(dp[i], 1 + dp[j])
+// After loops ends, Output [dp(n-1)]
+// Time Complexity: O(n^2)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = 0; i < b; i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+
+const int N = 1e5 + 2, MOD = 1e9 + 7;
+int dp[N];
+
+int lis(vi &a, int n)
+{
+    if (dp[n] != -1)
+        return dp[n];
+    dp[n] = 1; // single element is also an lis
+
+    rep(i, 0, n)
+    {
+        if (a[n] > a[i])
+            dp[n] = max(dp[n], 1 + lis(a, i));
+    }
+
+    return dp[n];
+}
+
+signed main()
+{
+    rep(i, 0, N)
+        dp[i] = -1;
+
+    int n;
+    cin >> n;
+
+    vi a(n);
+    rep(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    cout << lis(a, n - 1) << endl;
+
+    return 0;
+}
