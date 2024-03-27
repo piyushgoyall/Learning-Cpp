82e23cfe9fe9097a8ef681fb0e6742baf918c03d
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-03-27 23:52:47 +0530

    new

--- new/prac/6_double_pointer.cpp
+++ new/prac/6_double_pointer.cpp
@@ -2,14 +2,52 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+void update(int **p2)
+{
+    // p2 = p2 + 1;
+    // // anything changes? NO
+
+    // *p2 = *p2 + 1;
+    // // anything changes?
+
+    **p2 = **p2 + 1;
+    // anything changes?
+}
+
 int main()
 {
     int i = 5;
     int *p = &i;
     int **p2 = &p;
 
-    cout << "ALL OK" << endl;
-    cout << "Printing p: " << p << endl;
-    cout << "Address of p: " << &p << endl;
+    /*
+        cout << "ALL OK" << endl;
+        cout << "Printing p: " << p << endl;
+        // cout << "Address of p: " << &p << endl;
+
+        // Print value of i (different methods)
+        cout << *p << endl;
+        cout << i << endl;
+        cout << *p << endl;
+        cout << **p2 << endl;
+
+        // Print value of p (different methods)
+        cout << &i << endl;
+        cout << p << endl;
+        cout << *p2 << endl;
+
+        cout << &p << endl;
+        cout << p2 << endl;
+    */
+
+    cout << "Before: " << i << endl;
+    cout << "Before: " << p << endl;
+    cout << "Before: " << p2 << endl;
+    update(p2);
+    cout << "After: " << i << endl;
+    cout << "After: " << p << endl;
+    cout << "After: " << p2 << endl;
+
     return 0;
 }
