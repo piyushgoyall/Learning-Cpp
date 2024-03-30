2051ba0915db9a3698948d239f6d4473aaf60f5b
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-03-30 23:39:58 +0530

    new

--- new/prac/2_arr/7_arr_swap_alt.cpp
+++ new/prac/2_arr/7_arr_swap_alt.cpp
@@ -1,9 +1,7 @@
-// Swap alternate elements in an array.
-
 #include <bits/stdc++.h>
 using namespace std;
 
-void printArr(int arr[], int n)
+void printArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -13,27 +11,56 @@ void printArr(int arr[], int n)
 
 void altSwap(int arr[], int n)
 {
-    for (int i = 0; i < n; i += 2)
+    for (int i = 0; i < n - 1; i += 2)
     {
-        if (i + 1 < n)
-        {
-            swap(arr[i], arr[i + 1]);
-        }
+        swap(arr[i], arr[i + 1]);
     }
 }
 
 int main()
 {
-    int arr[6] = {1, 3, 2, 7, 11, 8};
-    int brr[5] = {11, 33, 9, 76, 43};
-
-    altSwap(arr, 6);
-    printArr(arr, 6);
-
-    cout << endl;
-
-    altSwap(brr, 5);
-    printArr(brr, 5);
-
+    int arr[5] = {1, 4, 3, 5, 8};
+    altSwap(arr, 5);
+    printArray(arr, 5);
     return 0;
 }
+
+// // Swap alternate elements in an array.
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void printArr(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// void altSwap(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i += 2)
+//     {
+//         if (i + 1 < n)
+//         {
+//             swap(arr[i], arr[i + 1]);
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int arr[6] = {1, 3, 2, 7, 11, 8};
+//     int brr[5] = {11, 33, 9, 76, 43};
+
+//     altSwap(arr, 6);
+//     printArr(arr, 6);
+
+//     cout << endl;
+
+//     altSwap(brr, 5);
+//     printArr(brr, 5);
+
+//     return 0;
+// }
