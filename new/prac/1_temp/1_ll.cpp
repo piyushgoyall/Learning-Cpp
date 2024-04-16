a47da839331454cdf092ac8ed567511fad303d0a
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-04-16 23:47:32 +0530

    new

--- new/prac/1_temp/1_ll.cpp
+++ new/prac/1_temp/1_ll.cpp
@@ -3,10 +3,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node
+{
+    int data;
+    Node *next;
+    Node *prev;
 
+    Node()
+    {
+        next = nullptr;
+        prev = nullptr;
+    }
+}
+
+class Dll
+{
+    Node *head;
+    Dll()
+    {
+        head = nullptr;
+    }
+
+    
+}
 
 int main()
 {
+    DoublyLinkedList list;
+    for (int i = 1; i <= 5; i++)
+    {
+        list.insertAtHead(i); // inserting data in list
+        list.printList();
+    }
+
+    list.Delete(1); // deleting 1 from the list
+    list.printList();
+
+    list.Delete(5); // deleting 5 from the list
+    list.printList();
+
+    list.Delete(3); // deleting 3 from the list
+    list.printList();
+
+    list.Delete(12);
+    list.printList();
 
     return 0;
 }
---
+++ new/prac/8_pointer/7_MCQ.cpp
@@ -0,0 +1,115 @@
+// Pointer MCQ
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+
+    /*
+
+    int arr[] = {11, 21, 31, 41};
+    int *ptr = arr++;
+    cout << *ptr << endl;
+
+    */
+
+    /*
+
+    int arr[6] = {11, 21, 31};
+    int *p = arr;
+    cout << p[2] << endl;
+
+    */
+
+    /*
+
+    int arr[6] = {11, 21, 13};
+    cout << (arr + 1) << endl;
+
+    */
+
+    /*
+
+    int arr[6] = {11, 12, 31};
+    cout << arr << " " << &arr << endl;
+
+    */
+
+    /*
+
+    int arr[] = {11, 21, 13, 14};
+    cout << *(arr) << " " << *(arr + 1) << endl;
+
+    */
+
+    /*
+
+    int arr[5];
+    int *ptr;
+    cout << sizeof(arr) << " " << sizeof(ptr) << endl;
+
+    */
+
+    /*
+
+    float f = 12.5;
+    float p = 21.5;
+    float *ptr = &f;
+    (*ptr)++;
+    *ptr = p;
+    cout << *ptr << " " << f << " " << p << endl;
+
+    */
+
+    /*
+
+    int first = 8;
+    int second = 11;
+    int *third = &second;
+    first = *third;
+    *third = *third + 2;
+    cout << first << " " << second << endl;
+
+    */
+
+    /*
+
+    int *p = 0;
+    int first = 110;
+    *p = first;
+    cout << *p << endl;
+
+    */
+
+    /*
+
+        int first = 8;
+        int *p = &first;
+        cout << (*p)++ << " ";
+        cout << first << endl;
+
+        */
+
+    /*
+
+    int first = 6;
+    int *p = &first;
+    int *q = p;
+    (*q)++;
+    cout << first << endl;
+
+    */
+
+    /*
+
+        int first = 8;
+        int second = 18;
+        int *ptr = &second;
+        *ptr = 9;
+        cout << first << " " << second << endl;
+
+    */
+
+    return 0;
+}
