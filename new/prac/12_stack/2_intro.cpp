98bb34a2879ed74b687afbd5ece10e41a352a968
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-05-07 19:06:02 +0530

    new

--- new/prac/12_stack/2_intro.cpp
+++ new/prac/12_stack/2_intro.cpp
@@ -3,12 +3,88 @@ using namespace std;
 
 class Stack
 {
+public:
     // properties
-    
-}
+    int *arr;
+    int top;
+    int size;
+
+    // behaviour
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+            cout << "Stack Overflow" << endl;
+        }
+    }
+
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "Stack Underflow" << endl;
+        }
+    }
+
+    int peek()
+    {
+        if (top >= 0 && top < size)
+        {
+            return arr[top];
+        }
+        else
+        {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
 
 int main()
 {
-    
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+
+    cout << st.peek() << endl;
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
     return 0;
 }
