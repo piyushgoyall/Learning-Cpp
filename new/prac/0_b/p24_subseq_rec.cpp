906e72ae05e761f4c06313924c5c399f757fba5d
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-06-29 23:18:16 +0530

    new

---
+++ new/prac/0_b/p24_subseq_rec.cpp
@@ -0,0 +1,77 @@
+// Print subsequences of array using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printF(int ind, vector<int> &ds, vector<int> &arr)
+{
+    if (ind == arr.size())
+    {
+        for (auto it : ds)
+        {
+            cout << it << " ";
+        }
+        if (ds.size() == 0)
+        {
+            cout << "{}";
+        }
+        cout << endl;
+        return;
+    }
+
+    // take or pick the particular index into the subsequence
+    ds.push_back(arr[ind]);
+    printF(ind + 1, ds, arr);
+    ds.pop_back();
+
+    // not pick, or not take condition, this element is not added to your subsequence
+    printF(ind + 1, ds, arr);
+}
+
+
+
+int main()
+{
+    vector<int> arr = {3, 1, 2};
+    int n = 3;
+    vector<int> ds;
+    printF(0, ds, arr);
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void printF(int ind, vector<int> &ds, int arr[], int n)
+// {
+//     if (ind == n)
+//     {
+//         for (auto it : ds)
+//         {
+//             cout << it << " ";
+//         }
+//         if (ds.size() == 0)
+//         {
+//             cout << "{}";
+//         }
+//         cout << endl;
+//         return;
+//     }
+
+//     // take or pick the particular index into the subsequence
+//     ds.push_back(arr[ind]);
+//     printF(ind + 1, ds, arr, n);
+//     ds.pop_back();
+
+//     // not pick, or not take condition, this element is not added to your subsequence
+//     printF(ind + 1, ds, arr, n);
+// }
+
+// int main()
+// {
+//     int arr[] = {3, 1, 2};
+//     int n = 3;
+//     vector<int> ds;
+//     printF(0, ds, arr, n);
+//     return 0;
+// }
