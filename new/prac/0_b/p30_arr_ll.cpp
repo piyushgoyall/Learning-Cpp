4f602f7387ef343a3155f2d024c77712942f9c95
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-07-05 23:55:02 +0530

    new

--- new/prac/0_b/p30_arr_ll.cpp
+++ new/prac/0_b/p30_arr_ll.cpp
@@ -63,33 +63,3 @@ int main()
 
     return 0;
 }
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void bsort(int *arr, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
-        }
-    }
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-    int *arr = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    bsort(arr, n);
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    delete[] arr;
-    return 0;
-}
---
+++ new/prac/0_b/p32_arr_st.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+template <class T>
+class Stack
+{
+private:
+    int maxSize;
+    int top;
+    T *array;
+
+public:
+    Stack(int size) : maxSize(size), top(-1)
+    {
+        array = new T[size];
+    }
+
+    ~Stack()
+    {
+        delete[] array;
+    }
+
+    bool IsEmpty()
+    {
+        return top == -1;
+    }
+
+    bool IsFull()
+    {
+        return top == maxSize - 1;
+    }
+
+    T Top()
+    {
+        if (IsEmpty())
+            throw runtime_error("Stack is empty");
+        return array[top];
+    }
+
+    void Push(T value)
+    {
+        if (IsFull())
+            throw runtime_error("Stack is full");
+        array[++top] = value;
+    }
+
+    T Pop()
+    {
+        if (IsEmpty())
+            throw runtime_error("Stack is empty");
+        return array[top--];
+    }
+};
+int main()
+{
+    int size = 5;
+    Stack<int> *arrayOfStacks = new Stack<int>[size]
+    { Stack<int>(10), Stack<int>(10), Stack<int>(10), Stack<int>(10), Stack<int>(10) };
+
+    // Push elements into the stacks
+    arrayOfStacks[0].Push(10);
+    arrayOfStacks[0].Push(20);
+    arrayOfStacks[1].Push(30);
+    arrayOfStacks[2].Push(40);
+    arrayOfStacks[2].Push(50);
+    arrayOfStacks[2].Push(60);
+
+    // Pop and print elements from the stacks
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Stack " << i << ": ";
+        while (!arrayOfStacks[i].IsEmpty())
+        {
+            cout << arrayOfStacks[i].Pop() << " ";
+        }
+        cout << endl;
+    }
+
+    // Free up space
+    delete[] arrayOfStacks;
+
+    return 0;
+}
