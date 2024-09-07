c51a815aab701e50354b685fcc221d3d954ce540
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-09-07 23:52:52 +0530

    insert

--- new/prac/0_b/p45_Trie.cpp
+++ new/prac/0_b/p45_Trie.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ALPHABET_SIZE 26
@@ -60,7 +60,36 @@ int Trie::getIndex(char t)
 }
 
 // Function to insert a key,value pair in the Trie
-void Trie::insertNode(string key) {}
+void Trie::insertNode(string key)
+{
+    // Empty string is not allowed
+    if (key.empty())
+        return;
+
+    // using transform() function and ::tolower in STL to convert 'key' to lowercase
+    transform(key.begin(), key.end(), key.begin(), ::tolower);
+    TrieNode *currentNode = root;
+    int index = 0;
+
+    // Iterate the trie with the given character index,
+    // If the index points to NULL
+    // simply create a TrieNode and go down a level
+    for (int level = 0; level < key.length(); level++)
+    {
+        index = getIndex(key[level]);
+
+        if (currentNode->children[index] == NULL)
+        {
+            currentNode->children[index] = new TrieNode();
+            cout << key[level] << " inserted" << endl;
+        }
+        currentNode = currentNode->children[index];
+    }
+
+    // Mark the end character as leaf node
+    currentNode->markAsLeaf();
+    cout << "'" << key << "' inserted" << endl;
+}
 
 // Function to search given key in Trie
 bool Trie::searchNode(string key)
@@ -76,9 +105,27 @@ bool Trie::deleteNode(string key)
 
 int main()
 {
+    /*Trie *t = new Trie();
+  cout << "Index to insert a = " << t->getIndex('a') << endl;
+  cout << "Index to insert t = " << t->getIndex('t') << endl;*/
+
+    string keys[9] = {"the", "a", "there", "answer", "any", "by", "bye", "their", "abc"};
+
     Trie *t = new Trie();
-    cout << "Index to insert a = " << t->getIndex('a') << endl;
-    cout << "Index to insert t = " << t->getIndex('t') << endl;
+
+    cout << "Keys to insert:\n";
+    for (int i = 0; i < 9; i++)
+    {
+        cout << keys[i] << ", ";
+    }
+    cout << endl;
+
+    // Construct trie
+    for (int i = 0; i < 9; i++)
+    {
+        t->insertNode(keys[i]);
+        cout << "\"" << keys[i] << "\"" << "Inserted." << endl;
+    }
 
     return 0;
 }
