c9aaa0fa6c76b6ab5eb0e6c4fe80c7f02cefe9b5
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-10-18 00:03:47 +0530

    add

--- new/prac/0_b/p39_BST_6.cpp
+++ new/prac/0_b/p39_BST_6.cpp
@@ -1,136 +1,215 @@
-// Delete Node in BST
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 class Node
 {
 public:
-    int data;
-    Node *left;
-    Node *right;
-
-    Node()
-    {
-        data = 0;
-        left = nullptr;
-        right = nullptr;
-    }
-
-    Node(int val)
-    {
-        this->data = val;
-        left = nullptr;
-        right = nullptr;
-    }
+    int value;
+    Node *leftChild;
+    Node *rightChild;
+    Node();
+    Node(int val);
 };
 
-class makeBST
+class BinarySearchTree
 {
 private:
     Node *root;
 
 public:
-    makeBST()
+    BinarySearchTree();
+    BinarySearchTree(int rootValue);
+    Node *getRoot();
+    Node *insert(Node *currentNode, int val);
+    void insertBST(int val);
+    bool Delete(Node *currentNode, int value);
+    bool deleteBST(int value);
+    void inOrderPrint(Node *currentNode);
+};
+
+Node::Node()
+{
+    value = 0;
+    leftChild = NULL;
+    rightChild = NULL;
+}
+
+Node::Node(int val)
+{
+    value = val;
+    leftChild = NULL;
+    rightChild = NULL;
+}
+
+BinarySearchTree::BinarySearchTree()
+{
+    root = NULL;
+}
+
+BinarySearchTree::BinarySearchTree(int rootValue)
+{
+    root = new Node(rootValue);
+}
+
+Node *BinarySearchTree::getRoot()
+{
+    return root;
+}
+
+Node *BinarySearchTree::insert(Node *currentNode, int val)
+{
+    if (currentNode == NULL)
     {
-        root = nullptr;
+        return new Node(val);
+    }
+    else if (currentNode->value > val)
+    {
+        currentNode->leftChild = insert(currentNode->leftChild, val);
+    }
+    else
+    {
+        currentNode->rightChild = insert(currentNode->rightChild, val);
+    }
+    return currentNode;
+}
+
+void BinarySearchTree::insertBST(int value)
+{
+    if (getRoot() == NULL)
+    {
+        root = new Node(value);
+        return;
+    }
+    insert(this->getRoot(), value);
+}
+
+bool BinarySearchTree::Delete(Node *currentNode, int value)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    Node *parent; // To Store parent of currentNode
+    while (currentNode != NULL && (currentNode->value != value))
+    {
+        parent = currentNode;
+        if (currentNode->value > value)
+            currentNode = currentNode->leftChild;
+        else
+            currentNode = currentNode->rightChild;
     }
 
-    makeBST(int val)
+    if (currentNode == NULL)
+        return false;
+    else if (currentNode->leftChild == NULL && currentNode->rightChild == NULL)
     {
-        root = new Node(val);
-    }
-
-    Node *getRoot()
-    {
-        return root;
-    }
-
-    void insertNode(Node *root, int val)
-    {
-        if (root == nullptr)
+        // 1. Node is Leaf Node
+        // if that leaf node is the root (a tree with just root)
+        if (root->value == currentNode->value)
         {
-            root = new Node(val);
-            return;
+            delete root;
+            root = NULL;
+            return true;
         }
-
-        Node *temp;
-        Node *curr = root;
-        while (curr != nullptr)
+        else if (currentNode->value < parent->value)
         {
-            temp = curr;
-            if (val < curr->data)
-                curr = curr->left;
-            else
-                curr = curr->right;
-        }
-
-        if (val < temp->data)
-        {
-            temp->left = new Node(val);
+            delete parent->leftChild;
+            parent->leftChild = NULL;
+            return true;
         }
         else
-            temp->right = new Node(val);
-    }
-
-    void insert(int val)
-    {
-        insertNode(root, val);
-    }
-
-    Node *search(int val)
-    {
-        Node *curr = root;
-        while (curr != nullptr && curr->data != val)
         {
-            if (curr->data > val)
-                curr = curr->left;
-
-            else
-                curr = curr->right;
-        }
-        return curr;
-    }
-
-    Node *delNode(Node* root,int val)
-    {
-        if(root==nullptr)
-            return nullptr;
-
-        Node *curr = root;
-        Node *temp;
-        while(curr!=nullptr && curr->data!=val)
-        {
-            temp = curr;
-            if()
+            delete parent->rightChild;
+            parent->rightChild = NULL;
+            return true;
         }
     }
-
-    void delete(int val)
+    else if (currentNode->rightChild == NULL)
     {
-        delNode(root, val);
-    }
 
-    void printInorder(Node *curr)
-    {
-        if (curr != nullptr)
+        if (root->value == currentNode->value)
         {
-            printInorder(curr->left);
-            cout << curr->data << " ";
-            printInorder(curr->right);
+            delete root;
+            root = currentNode->leftChild;
+            return true;
+        }
+        else if (currentNode->value < parent->value)
+        {
+            delete parent->leftChild;
+            parent->leftChild = currentNode->leftChild;
+            return true;
+        }
+        else
+        {
+            delete parent->rightChild;
+            parent->rightChild = currentNode->leftChild;
+            return true;
         }
     }
-};
+    else if (currentNode->leftChild == NULL)
+    {
+
+        if (root->value == currentNode->value)
+        {
+            delete root;
+            root = currentNode->rightChild;
+            return true;
+        }
+        else if (currentNode->value < parent->value)
+        {
+            delete parent->leftChild;
+            parent->leftChild = currentNode->rightChild;
+            return true;
+        }
+        else
+        {
+            delete parent->rightChild;
+            parent->rightChild = currentNode->rightChild;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool BinarySearchTree::deleteBST(int value)
+{
+    return Delete(root, value);
+}
+
+void BinarySearchTree::inOrderPrint(Node *currentNode)
+{
+    if (currentNode != NULL)
+    {
+        inOrderPrint(currentNode->leftChild);
+        cout << currentNode->value << endl;
+        inOrderPrint(currentNode->rightChild);
+    }
+}
 
 int main()
 {
-    makeBST b(12);
-    b.insert(432);
-    b.insert(524);
-    b.insert(7467);
-    b.printInorder(b.getRoot());
-    cout << endl;
-    cout << (b.search(432) ? "Found" : "Not Found") << endl;
+
+    BinarySearchTree BST(13);
+
+    BST.insertBST(4);
+    BST.insertBST(7);
+    BST.insertBST(16);
+    BST.insertBST(19);
+    BST.insertBST(3);
+    BST.insertBST(1);
+    BST.insertBST(9);
+    BST.insertBST(2);
+    BST.insertBST(5);
+    BST.insertBST(15);
+
+    cout << "InOrderTraversal" << endl;
+    BST.inOrderPrint(BST.getRoot());
+
+    cout << "Deleting 4" << endl;
+    BST.deleteBST(4);
+
+    cout << "BST After deleting 4" << endl;
+    BST.inOrderPrint(BST.getRoot());
 
     return 0;
 }
