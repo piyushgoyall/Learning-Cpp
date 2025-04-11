71fd4e22b7567c93375ee0194872faa9a49d63a9
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2025-04-11 23:31:38 +0530

    new

---
+++ new/prac/0_b/p105_3_Sum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    set<vector<int>> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<vector<int>> ans = triplet(n, arr);
+    for (auto it : ans)
+    {
+        cout << "[";
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
